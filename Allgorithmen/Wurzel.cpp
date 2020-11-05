4abd7b4bf31e72e7b67d792b3d28db600e76e320
BenjaminS01 <bswarovsky@gmx.net>
2020-11-05 10:02:51 +0100

    Übung 1

---
+++ Allgorithmen/Wurzel.cpp
@@ -0,0 +1,38 @@
+#include "Wurzel.h"
+#include <math.h>
+#include <cmath>
+
+
+
+
+Wurzel::Wurzel()
+{
+}
+
+
+Wurzel::~Wurzel()
+{
+}
+
+double Wurzel::getWurzel(double a, int n)
+{
+	double predecessor = 0;
+	double result = 1;
+	bool isFirstRound = true;
+
+	while (abs((predecessor - result)) > 0.01) { // abs => Betrag
+		
+		predecessor = result;
+		result = ((n - 1)*pow(result, n) + a) / (n * pow(result, n-1) );
+
+		if (isFirstRound)
+		{
+			isFirstRound = false;
+			continue;
+		}
+	}
+	return result;
+}
+
+
+
---
+++ Allgorithmen/Wurzel.h
@@ -0,0 +1,10 @@
+
+class Wurzel
+{
+public:
+	Wurzel();
+	~Wurzel();
+
+public: double getWurzel(double a, int n);
+
+};
